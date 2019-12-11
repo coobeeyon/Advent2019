e476d9d27cd0aed7bd53fabdf54f8bb45e59aa4b
Michael Daum <michael.daum@epicgames.com>
2019-12-10 23:28:28 -0500

    Day 5 Part A Solved

--- Day5.cpp
+++ Day5.cpp
@@ -21,30 +21,116 @@ enum class OpCode : uint8_t
 	Halt   = 99
 };
 
+std::ostream&
+operator<<(std::ostream& ostream, const OpCode& opCode)
+{
+	switch (opCode)
+	{
+	case OpCode::Add:
+		ostream << "Add";
+		break;
+	case OpCode::Mult:
+		ostream << "Mult";
+		break;
+	case OpCode::Input:
+		ostream << "Input";
+		break;
+	case OpCode::Output:
+		ostream << "Output";
+		break;
+	case OpCode::Halt:
+		ostream << "Halt";
+		break;
+	}
+	return ostream;
+}
+
+int
+numParams( OpCode opCode )
+{
+	switch (opCode)
+	{
+	case OpCode::Add:
+	case OpCode::Mult:
+		return 3;
+	case OpCode::Input:
+	case OpCode::Output:
+		return 1;
+	case OpCode::Halt:
+		return 0;
+	}
+}
+
 enum class ParameterMode : uint8_t
 {
 	Position = 0,
 	Value    = 1
 };
 
+std::ostream&
+operator<<(std::ostream& ostream, const ParameterMode& mode)
+{
+	switch (mode)
+	{
+	case ParameterMode::Position:
+		ostream << "Position";
+		break;
+	case ParameterMode::Value:
+		ostream << "Value";
+		break;
+	}
+	return ostream;
+}
+
 struct Parameter
 {
-	int           m_value;
-	ParameterMode m_mode;
+	int           m_value{};
+	ParameterMode m_mode{ParameterMode::Position};
 };
+
+std::ostream&
+operator<<(std::ostream& ostream, const Parameter& parameter)
+{
+	ostream << "(" << parameter.m_value << "," << parameter.m_mode << ")";
+	return ostream;
+}
+using Parameters = std::vector<Parameter>;
+
+struct Instruction
+{
+	OpCode m_opCode{OpCode::Halt};
+	Parameters m_parameters;
+};
+
+std::ostream&
+operator<<(std::ostream& ostream, const Instruction& instruction)
+{
+	ostream << "[ " << instruction.m_opCode ;
+	for ( const auto& param : instruction.m_parameters )
+	{
+		ostream << param << " ";
+	}
+	ostream << "]";
+
+	return ostream;
+}
 
 class Runtime
 {
 public:
 	Runtime(Program&& program):
 		m_program(std::move(program)),
-		m_currentPos(m_program.begin()),
+		m_instructionPointer(m_program.begin()),
 		m_isRunning(false)
 	{}
 	void run();
 private:
+	Instruction nextInstruction();
+	void executeInstruction(const Instruction& instruction);
+	int getParameter(const Parameter& parameter);
+
 	Program m_program{};
-	Program::iterator m_currentPos{};
+	Program::iterator m_instructionPointer{};
 	bool m_isRunning{};
 };
 
@@ -52,9 +138,68 @@ void
 Runtime::run()
 {
 	m_isRunning = true;
-	std::copy(m_program.begin(), m_program.end(),
-			  std::ostream_iterator<int>(std::cout));
-	m_isRunning = false;
+	while( m_isRunning )
+	{
+		Instruction instruction = nextInstruction();
+		std::cout << instruction << std::endl;
+		executeInstruction(instruction);
+	}
+}
+
+Instruction
+Runtime::nextInstruction()
+{
+	Instruction ret{};
+	int instruction = *m_instructionPointer++;
+	ret.m_opCode = static_cast<OpCode>(instruction % 100);
+	instruction = instruction / 100;
+	ret.m_parameters.resize( numParams(ret.m_opCode) );
+	for ( auto& param : ret.m_parameters )
+	{
+		int value = *m_instructionPointer++;
+		ParameterMode parameterMode = static_cast<ParameterMode>(instruction % 10);
+		param = Parameter{value, parameterMode};
+		instruction = instruction / 10;
+	}
+	return ret;
+}
+
+void
+Runtime::executeInstruction(const Instruction& instruction)
+{
+	switch ( instruction.m_opCode )
+	{
+	case OpCode::Add:
+		m_program[ instruction.m_parameters[2].m_value ] =
+			getParameter(instruction.m_parameters[0]) + getParameter(instruction.m_parameters[1]);
+		break;
+	case OpCode::Mult:
+		m_program[ instruction.m_parameters[2].m_value ] =
+			getParameter(instruction.m_parameters[0]) * getParameter(instruction.m_parameters[1]);
+		break;
+	case OpCode::Input:
+		std::cin >> m_program[ instruction.m_parameters[0].m_value ];
+		break;
+	case OpCode::Output:
+		std::cout << getParameter(instruction.m_parameters[0]) << std::endl;
+		break;
+	case OpCode::Halt:
+		m_isRunning = false;
+		break;
+	}
+}
+
+int
+Runtime::getParameter(const Parameter& parameter)
+{
+	if ( parameter.m_mode == ParameterMode::Position )
+	{
+		return m_program[parameter.m_value];
+	}
+	else
+	{
+		return parameter.m_value;
+	}
 }
 
 std::vector<int> loadProgram(const std::string& fileName)
